b36381af52820ecaf2015218dc12c13b526eeea2
Atumonye James <154873745+Jamestown34@users.noreply.github.com>
2025-07-01 11:07:47 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,30 @@
+// app/page.tsx
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Profile from './components/Profile';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Dashboards from './components/Dashboards';
+import Certifications from './components/Certifications';
+import Courses from './components/Courses';
+import Contact from './components/Contact';
 
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="pt-[17vh] md:pt-[17vh]"> {/* Adjust padding-top to account for fixed navbar height */}
+        <Profile />
+        <About />
+        <Experience />
+        <Projects />
+        <Dashboards />
+        <Certifications />
+        <Courses />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
