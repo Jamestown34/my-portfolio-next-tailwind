59e92ab480d94db0bb9c524444214768f36fdad3
Atumonye James <154873745+Jamestown34@users.noreply.github.com>
2025-07-01 09:27:25 +0100

    Create AboutContent.tsx

---
+++ app/components/AboutContent.tsx
@@ -0,0 +1,31 @@
+// app/components/AboutContent.tsx
+import React from 'react';
+
+export default function AboutContent() {
+  return (
+    <div className="text_container text-center md:text-left">
+      <p className="mb-4 text-text2">
+        A Data Scientist with a background in Pharmaceutical Technology. My journey into data science started
+        during my NYSC program at the Ekiti State Ministry of Health, where I became fascinated by the flow of
+        data and its potential in decision-making. That experience sparked my curiosity and led me to pursue a
+        career in data analytics.
+      </p>
+      <p className="mb-4 text-text2">
+        To build my skills, I have taken courses from Coursera, Udemy, and Alex The Analyst, always looking for
+        new ways to expand my knowledge. I actively participate in hackathons and engage with data science
+        communities, sharpening my ability to uncover trends and make sense of complex data. Predictive
+        analytics and machine learning excite me the most because they allow me to turn raw data into valuable insights.
+      </p>
+      <p className="mb-4 text-text2">
+        Balancing my background in Pharmaceutical with my enthusiasm for data science keeps me engaged in both
+        fields, exploring ways to bridge the gap between business, finance, healthcare, and analytics. Outside of
+        work, I have a deep love for history, enjoy movies, and often imagine myself playing the piano.
+      </p>
+      <p className="text-text2">
+        My goal is to become a highly skilled data scientist, making an impact in business, tech firms, finance,
+        e-commerce, healthcare, and possibly a tech startup by driving data-driven innovation. Feel free to
+        check out my projects below, where I share my experiences and insights.
+      </p>
+    </div>
+  );
+}
