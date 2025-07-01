40785dc92787ba662d9936e60df6e47068431bcc
Atumonye James <154873745+Jamestown34@users.noreply.github.com>
2025-07-01 09:32:15 +0100

    Create SkillCard.tsx

---
+++ app/components/SkillCard.tsx
@@ -0,0 +1,20 @@
+// app/components/SkillCard.tsx
+import Image from 'next/image';
+import React from 'react'; // Import React for React.ReactNode
+
+interface SkillCardProps {
+  title: string;
+  level: string;
+}
+
+export default function SkillCard({ title, level }: SkillCardProps) {
+  return (
+    <article className="flex items-center w-40 justify-start gap-2"> {/* Adjusted width and alignment */}
+      <Image src="/images/checkmarkgood.jpg" alt="Experience icon" className="icon w-8 h-8" width={32} height={32} />
+      <div>
+        <h3 className="text-text1 font-medium">{title}</h3>
+        <p className="text-text2 text-sm">{level}</p>
+      </div>
+    </article>
+  );
+}
